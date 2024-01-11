f7c44df1067c51ae60943cdbe4a6a9f2dece4245
QMXG1WG_thdgit <fernando_ku@homedepot.com>
2024-01-11 12:42:37 -0600

    test

--- ui-ktx/build.gradle.kts
+++ ui-ktx/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.com.android.library)
     alias(libs.plugins.org.jetbrains.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -30,4 +31,18 @@ android {
 
 dependencies {
 
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.fernandohkuku"
+                artifactId = "ui-ktx-preview-test"
+                version = "1.0"
+            }
+        }
+    }
 }
